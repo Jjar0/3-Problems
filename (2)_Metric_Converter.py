7accc1d281256ed5b050150363fb626d7f0d1fe1
Peter Hayward <peterhayward1707@gmail.com>
2023-11-28 18:02:43 +0000

    Overhauled metric converter

--- (2)_Metric_Converter.py
+++ (2)_Metric_Converter.py
@@ -7,116 +7,65 @@ def menu():
     print ("[3] Yards to Meters")
     print ("[4] Inches to Centimeters")
 
-    menuSelection = input (">")
+    menuSelection = input (">") #menu function to choose conversion rate.
+
+    conversionRate = 0
+    imperial = ""
+    metric = ""
 
     if menuSelection == "1":
-        miles()
+        conversionRate = 1.6
+        imperial = "miles"
+        metric = "kilometers"
+        convert(conversionRate,imperial,metric)
+        
     if menuSelection == "2":
-        feet()
+        conversionRate = 30.48
+        imperial = "feet"
+        metric = "meters"
+        convert(conversionRate,imperial,metric)
+        
     if menuSelection == "3":
-        yards()
+        conversionRate = 0.9144
+        imperial = "yards"
+        metric = "meters"
+        convert(conversionRate,imperial,metric)
+        
     if menuSelection == "4":
-        inches()
+        conversionRate = 2.54
+        imperial = "inches"
+        metric = "centimeters"
+        convert(conversionRate,imperial,metric)
+        
     else:
         print ("Please enter one of the numbers listed above")
         menu()
     
-def miles():
+def convert(conversionRate,imperial,metric):
+
+    conversionRate = float(conversionRate)
     
     print ("enter 0 to exit to the menu")
-    print ("Please enter a value (in miles)")
-    milesValue = input (">")
+    print ("Please enter a value in " + imperial)
+    inp = input (">")
 
-    if milesValue == "0":
+    if inp == "0":
         menu()
 
     try:
-        milesValue = float(milesValue)
+        inp = float(inp)
     except:
         print ("Please enter a numeric value")
-        miles()
+        convert(conversionRate,imperial,metric)
 
-    milesValue = float(milesValue)
-    kiloValue = (milesValue * 1.6)
+    inp = float(inp)
+    out = (inp * conversionRate)
     
-    milesValue = str(milesValue)
-    kiloValue = str(kiloValue)
+    inp = str(inp)
+    out = str(out)
 
-    print (milesValue + " miles is " + kiloValue + " in Kilometers.")
-    miles()   
-
-def feet():
-    
-    print ("enter 0 to exit to the menu")
-    print ("Please enter a value (in feet)")
-    feetValue = input (">")
-
-    if feetValue == "0":
-        menu()
-
-    try:
-        feetValue = float(feetValue)
-    except:
-        print ("Please enter a numeric value")
-        feet()
-
-    feetValue = float(feetValue)
-    cmValue = (feetValue * 30.48)
-    
-    feetValue = str(feetValue)
-    cmValue = str(cmValue)
-
-    print (feetValue + " feet is " + cmValue + " in centimeters.")
-    feet()   
-
-def yards():
-    
-    print ("enter 0 to exit to the menu")
-    print ("Please enter a value (in yards)")
-    yardsValue = input (">")
-
-    if yardsValue == "0":
-        menu()
-
-    try:
-        yardsValue = float(yardsValue)
-    except:
-        print ("Please enter a numeric value")
-        yards()
-
-    yardsValue = float(yardsValue)
-    mValue = (yardsValue * 0.9144)
-    
-    yardsValue = str(yardsValue)
-    mValue = str(mValue)
-
-    print (yardsValue + " yards is " + mValue + " in meters.")
-    yards()   
-
-def inches():
-    
-    print ("enter 0 to exit to the menu")
-    print ("Please enter a value (in inches)")
-    inchValue = input (">")
-
-    if inchValue == "0":
-        menu()
-
-    try:
-        inchValue = float(inchValue)
-    except:
-        print ("Please enter a numeric value")
-        inches()
-
-    inchValue = float(inchValue)
-    cmValue = (inchValue * 2.54)
-    
-    inchValue = str(inchValue)
-    cmValue = str(cmValue)
-
-    print (inchValue + " inches is " + cmValue + " in centimeters.")
-    inches()
-
+    print (inp + " " + imperial + " is " + out + " in " + metric + ".")
+    convert(conversionRate,imperial,metric)  
 
 print ("Welcome to the imperial to metric unit converter!")
 menu()
