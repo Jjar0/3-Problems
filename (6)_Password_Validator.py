69e94a5e21791a13cdf3ee51697243b89a732630
Peter Hayward <peterhayward1707@gmail.com>
2023-11-28 02:40:28 +0000

    Fixed logic error

--- (6)_Password_Validator.py
+++ (6)_Password_Validator.py
@@ -4,31 +4,40 @@ def entry():
     print ("\nPlease enter your password")
     password = input (">")
 
+    valid = True
+
     if len(password) > 16:  
         print("Password must be no more than 16 characters")
+        valid = False
 
     if len(password) < 6:
         print ("Password must be a minimum of 6 characters")
+        valid = False
 
     if not any (char.isdigit() for char in password):
         print ("Password must contain at least 1 number")
+        valid = False
         
 
     if not any (char.isupper() for char in password):
         print ("Password must contain at least 1 uppercase letter")
+        valid = False
         
 
     if not any (char.islower() for char in password):
         print ("Password must contain at least 1 lowercase letter") 
+        valid = False
         
 
     if not any (not char.isalnum() for char in password): #for loops to check for non-alphanumeric (special) characters in the string
         print ("Password must contain at least 1 special character")
-        
-        entry()
-    else:
+        valid = False
+
+    if valid == True:
         print ("\nYour password is valid!")
-        entry()
+        
+    entry()
+    
     
 
 
