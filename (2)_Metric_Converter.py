b7b508accaab7620101280d842e49dbf69665333
Peter Hayward <149413456+Jjar0@users.noreply.github.com>
2023-12-01 00:15:58 +0000

    Post test commenting and fixes

--- (2)_Metric_Converter.py
+++ (2)_Metric_Converter.py
@@ -1,7 +1,7 @@
 
 def menu():
     
-    print ("Which units would to like to convert?")
+    print ("Which units would to like to convert?") #output of main menu for user.
     print ("[1] Miles to Kilometers")
     print ("[2] Feet to Meters")
     print ("[3] Yards to Meters")
@@ -9,13 +9,13 @@ def menu():
 
     menuSelection = input (">") #menu function to choose conversion rate.
 
-    conversionRate = 0
+    conversionRate = 0 #variable defenitions.
     imperial = ""
     metric = ""
 
     if menuSelection == "1":
-        conversionRate = 1.6 #conversion rate and measurements are stored in variables for later use.
-        imperial = "miles"
+        conversionRate = 1.6 #conversion rate and measurements are stored in variables for later use. 
+        imperial = "miles"  #when condition is true, values are assigned to variables and cvarried through to convert function
         metric = "kilometers"
         convert(conversionRate,imperial,metric)
         
@@ -38,32 +38,32 @@ def menu():
         convert(conversionRate,imperial,metric)
         
     else:
-        print ("Please enter one of the numbers listed above") #basic validation.
+        print ("Please enter one of the numbers listed above") #basic validation to check for invalid code.
         menu()
     
 def convert(conversionRate,imperial,metric): #function is used to loop converter.
 
-    conversionRate = float(conversionRate)
+    conversionRate = float(conversionRate) #data stype casting so that conversion rates can be operated in maths.
     
     print ("enter 0 to exit to the menu")
-    print ("Please enter a value in " + imperial) 
+    print ("Please enter a value in " + imperial) #tells user to input the data in thier chosen imperial unit via variable.
     inp = input (">")
 
-    if inp == "0":
+    if inp == "0": #exit condition to return to menu at any time.
         menu()
 
     try:
-        inp = float(inp) #validation for user input as a float value
+        inp = float(inp) #validation for user input as a float value.
     except:
         print ("Please enter a numeric value")
-        convert(conversionRate,imperial,metric)
+        convert(conversionRate,imperial,metric) #uses function to loop upon non float entry.
 
     inp = float(inp)
-    out = (inp * conversionRate) #calculation 
+    out = (inp * conversionRate) #calculation conversion to find metric value.
     
     #imperial/metric strings are used so output message defines which numbers are which measurements
-    print (str(inp) + " " + imperial + " is " + "%.2f" % out + " in " + metric + ".")
+    print (str(inp) + " " + imperial + " is " + "%.2f" % out + " in " + metric + ".") #[REVISION] string formatting keeps outputted values to 2 decimal places for easier readability.
     convert(conversionRate,imperial,metric)  
 
 print ("Welcome to the imperial to metric unit converter!")
-menu()
+menu() #restart game
